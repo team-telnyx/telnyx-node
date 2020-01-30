92b4ee0ca0cb1db202d193389ce4f8d93b133f02
Lucas <lucas@telnyx.com>
2020-01-29 21:28:13 -0300

    feat(resources): add Ips and Fqdns Resources (#25)
    * add Ips Resource
    
    * add Fqdns Resource
    
    * target new telnyx-mock version

--- test/resources/Fqdns.spec.js
+++ test/resources/Fqdns.spec.js
@@ -5,18 +5,18 @@ var expect = require('chai').expect;
 
 var TEST_AUTH_KEY = 'KEY187557EC22404DB39975C43ACE661A58_9QdDI7XD5bvyahtaWx1YQo';
 
-describe('IpConnections Resource', function() {
+describe('Fqdns Resource', function() {
   describe('retrieve', function() {
     function responseFn(response) {
       expect(response.data).to.include({id: '123'});
     }
 
     it('Sends the correct request', function() {
-      return telnyx.ipConnections.retrieve('123').then(responseFn);
+      return telnyx.fqdns.retrieve('123').then(responseFn);
     })
 
     it('Sends the correct request [with specified auth]', function() {
-      return telnyx.ipConnections.retrieve('123', TEST_AUTH_KEY)
+      return telnyx.fqdns.retrieve('123', TEST_AUTH_KEY)
         .then(responseFn);
     });
   });
@@ -24,23 +24,24 @@ describe('IpConnections Resource', function() {
   describe('create', function() {
     function responseFn(response) {
       expect(response.data).to.have.property('id');
-      expect(response.data).to.have.property('connection_name');
+      expect(response.data).to.have.property('connection_id');
+      expect(response.data).to.have.property('fqdn');
       expect(response.data).to.have.property('record_type');
-      expect(response.data).to.include({connection_name: 'Central BSD-1', record_type: 'ip_connection'});
+      expect(response.data).to.include({connection_id: 'Central BSD-1', record_type: 'fqdn'});
     }
 
     it('Sends the correct request', function() {
-      return telnyx.ipConnections.create({connection_name: 'Central BSD-1'})
+      return telnyx.fqdns.create({connection_id: 'Central BSD-1', fqdn: 'example.com'})
         .then(responseFn);
     })
 
     it('Sends the correct request [with specified auth]', function() {
-      return telnyx.ipConnections.create({connection_name: 'Central BSD-1'}, TEST_AUTH_KEY)
+      return telnyx.fqdns.create({connection_id: 'Central BSD-1', fqdn: 'example.com'}, TEST_AUTH_KEY)
         .then(responseFn);
     });
 
     it('Sends the correct request [with specified auth in options]', function() {
-      return telnyx.ipConnections.create({connection_name: 'Central BSD-1'}, {api_key: TEST_AUTH_KEY})
+      return telnyx.fqdns.create({connection_id: 'Central BSD-1', fqdn: 'example.com'}, {api_key: TEST_AUTH_KEY})
         .then(responseFn);
     });
   });
@@ -48,17 +49,18 @@ describe('IpConnections Resource', function() {
   describe('list', function() {
     function responseFn(response) {
       expect(response.data[0]).to.have.property('id');
-      expect(response.data[0]).to.have.property('connection_name');
-      expect(response.data[0]).to.include({record_type: 'ip_connection'});
+      expect(response.data[0]).to.have.property('connection_id');
+      expect(response.data).to.have.property('fqdn');
+      expect(response.data[0]).to.include({record_type: 'fqdn'});
     }
 
     it('Sends the correct request', function() {
-      return telnyx.ipConnections.list()
+      return telnyx.fqdns.list()
         .then(responseFn);
     });
 
     it('Sends the correct request [with specified auth]', function() {
-      return telnyx.ipConnections.list(TEST_AUTH_KEY)
+      return telnyx.fqdns.list(TEST_AUTH_KEY)
         .then(responseFn);
     });
   });
@@ -67,25 +69,26 @@ describe('IpConnections Resource', function() {
     function responseFn(response) {
       if (response.data) {
         expect(response.data).to.have.property('id');
-        expect(response.data).to.have.property('connection_name');
-        expect(response.data).to.include({record_type: 'ip_connection'});
+        expect(response.data).to.have.property('connection_id');
+        expect(response.data).to.have.property('fqdn');
+        expect(response.data).to.include({record_type: 'fqdn'});
       }
     }
 
     describe('del', function() {
       it('Sends the correct request', function() {
-        return telnyx.ipConnections.create({connection_name: 'Central BSD-1'})
+        return telnyx.fqdns.create({connection_id: 'Central BSD-1', fqdn: 'example.com'})
           .then(function(response) {
-            const ipConnection = response.data;
-            return ipConnection.del()
+            const fqdn = response.data;
+            return fqdn.del()
               .then(responseFn);
           })
       });
       it('Sends the correct request [with specified auth]', function() {
-        return telnyx.ipConnections.retrieve('123')
+        return telnyx.fqdns.retrieve('123')
           .then(function(response) {
-            const ipConnection = response.data;
-            return ipConnection.del(TEST_AUTH_KEY)
+            const fqdn = response.data;
+            return fqdn.del(TEST_AUTH_KEY)
               .then(responseFn);
           })
       });
@@ -93,18 +96,18 @@ describe('IpConnections Resource', function() {
 
     describe('update', function() {
       it('Sends the correct request', function() {
-        return telnyx.ipConnections.create({connection_name: 'Central BSD-1'})
+        return telnyx.fqdns.create({connection_id: 'Central BSD-1', fqdn: 'example.com'})
           .then(function(response) {
-            const ipConnection = response.data;
-            return ipConnection.update({connection_name: 'Western BSD-2'})
+            const fqdn = response.data;
+            return fqdn.update({connection_id: 'Western BSD-2'})
               .then(responseFn);
           })
       });
       it('Sends the correct request [with specified auth]', function() {
-        return telnyx.ipConnections.retrieve('123')
+        return telnyx.fqdns.retrieve('123')
           .then(function(response) {
-            const ipConnection = response.data;
-            return ipConnection.update({connection_name: 'Western BSD-2'}, TEST_AUTH_KEY)
+            const fqdn = response.data;
+            return fqdn.update({connection_id: 'Western BSD-2'}, TEST_AUTH_KEY)
               .then(responseFn);
           })
       });
