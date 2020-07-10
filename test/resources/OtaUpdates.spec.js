993b77008fafe40ea2462fdcc855f729e7f1032c
Lucas Rosa <lucas@telnyx.com>
2020-07-10 10:51:02 -0300

    feat(resources): OTA Updates (#40)
    * fix outdated specs
    
    * update telnyx-mock target version
    
    * add OTA Updates Resource

---
+++ test/resources/OtaUpdates.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var utils = require('../../testUtils');
+var telnyx = utils.getTelnyxMock();
+var expect = require('chai').expect;
+
+var TEST_AUTH_KEY = utils.getUserTelnyxKey();
+
+describe('OtaUpdates Resource', function() {
+  describe.skip('retrieve', function() {
+    function responseFn(response) {
+      expect(response.data).to.include.keys(['id', 'sim_card_id', 'status', 'settings']);
+      expect(response.data).to.include({
+        record_type: 'ota_update',
+        type: 'sim_card_network_preferences'
+      });
+    }
+
+    it('Sends the correct request', function() {
+      return telnyx.otaUpdates.retrieve('123')
+        .then(responseFn);
+    });
+
+    it('Sends the correct request [with specified auth]', function() {
+      return telnyx.otaUpdates.retrieve('123', TEST_AUTH_KEY)
+        .then(responseFn);
+    });
+  });
+
+  describe('list', function() {
+    function responseFn(response) {
+      expect(response.data[0]).to.include.keys(['id', 'sim_card_id', 'status']);
+      expect(response.data[0]).to.include({
+        record_type: 'ota_update',
+        type: 'sim_card_network_preferences'
+      });
+    }
+
+    it('Sends the correct request', function() {
+      return telnyx.otaUpdates.list()
+        .then(responseFn);
+    });
+
+    it('Sends the correct request [with specified auth]', function() {
+      return telnyx.otaUpdates.list(TEST_AUTH_KEY)
+        .then(responseFn);
+    });
+  });
+});
