148a80f3edb7517559b12dd03597e6a06b674683
vionash <quagmilion@gmail.com>
2021-03-19 13:29:08 -0500

    Revert "Revert "Revert "fix #2"""
    This reverts commit ee2749dcc2e5ab3270a1accaca11410503a4eeeb.

--- test/resources/MessagingHostedNumbers.spec.js
+++ test/resources/MessagingHostedNumbers.spec.js
@@ -4,22 +4,24 @@ var utils = require('../../testUtils');
 var telnyx = utils.getTelnyxMock();
 var expect = require('chai').expect;
 
+var TEST_AUTH_KEY = utils.getUserTelnyxKey();
+
 describe('del', function() {
 
-  function responseFn(response) {
-    if (response.data) {
-      expect(response.data).to.have.property('id');
-      expect(response.data).to.include({record_type: 'messaging_hosted_number'});
+    function responseFn(response) {
+      if (response.data) {
+        expect(response.data).to.have.property('id');
+        expect(response.data).to.include({record_type: 'messaging_hosted_number'});
+      }
     }
-  }
 
-  it('Correct del method', function() {
-    return telnyx.messagingHostedNumberOrders.create({'messaging_profile_id': '123456', 'phone_numbers': '+11234567890'})
-      .then(function(response) {
-        const messagingHostedNumbers = response.data;
-        messagingHostedNumbers.id = '123123';
-        return messagingHostedNumbers.del()
-          .then(responseFn);
-      })
-  });
+    it('Correct del method', function() {
+      return telnyx.messagingHostedNumberOrders.create({'messaging_profile_id': '123456', 'phone_numbers': '+11234567890'})
+        .then(function(response) {
+          const messagingHostedNumbers = response.data;
+          messagingHostedNumbers.id = '123123';
+          return messagingHostedNumbers.del()
+            .then(responseFn);
+        })
+    });
 });
