f6fd928bbc79d16c0d40f176f43d52ce1c9b251d
Drew Kitch <drew@telnyx.com>
2021-09-15 13:43:56 -0600

    additional support for conference calls

--- test/resources/Conferences.spec.js
+++ test/resources/Conferences.spec.js
@@ -12,7 +12,17 @@ var CONFERENCES = [
   'unmute',
   'hold',
   'unhold',
+  'speak',
+  'play',
+  'stop',
+  'record_start',
+  'record_stop',
+  'update',
+  'leave',
+  'dial_participant',
 ]
+
+var CONFERENCE_ID = '41b9acd4-f4da-4ff5-a85c-e07e90b53f46';
 
 describe('Calls Resource', function() {
   describe('Call Conferences', function() {
@@ -22,10 +32,34 @@ describe('Calls Resource', function() {
     }
     const callConferencesData = {
       join: {
-        call_control_id: ''
+        call_control_id: '891510ac-f3e4-11e8-af5b-de00688a4901'
       },
       unhold: {
         call_control_ids: ['891510ac-f3e4-11e8-af5b-de00688a4901']
+      },
+      speak: {
+        language: 'en-US',
+        payload: 'Say this to participants',
+        voice: 'female'
+      },
+      play: {
+        media_name: 'my_media_uploaded_to_media_storage_api'
+      },
+      record_start: {
+        channels: 'single',
+        format: 'wav'
+      },
+      update: {
+        call_control_id: '891510ac-f3e4-11e8-af5b-de00688a4901',
+        supervisor_role: 'whisper'
+      },
+      leave: {
+        call_control_id: '891510ac-f3e4-11e8-af5b-de00688a4901'
+      },
+      dial_participant: {
+        call_control_id: '891510ac-f3e4-11e8-af5b-de00688a4901',
+        from: '+15555555555',
+        to: '+16666666666'
       }
     }
 
@@ -50,6 +84,23 @@ describe('Calls Resource', function() {
       });
     });
 
+    describe('retrieve', function() {
+      function responseFn(response) {
+        expect(response.data).to.have.property('id');
+        expect(response.data).to.have.property('name');
+        expect(response.data).to.include({record_type: 'conference'});
+      }
+
+      it('Sends the correct request', function() {
+        return telnyx.conferences.retrieve(CONFERENCE_ID)
+          .then(responseFn);
+      });
+      it('Sends the correct request [with specified auth]', function() {
+        return telnyx.conferences.retrieve(CONFERENCE_ID, TEST_AUTH_KEY)
+          .then(responseFn);
+      });
+    });
+
     describe('create', function() {
       function responseFn(response) {
         expect(response.data).to.include({
@@ -66,6 +117,29 @@ describe('Calls Resource', function() {
 
       it('Sends the correct request [with specified auth]', function() {
         return telnyx.conferences.create(conferenceCreateData, TEST_AUTH_KEY)
+          .then(responseFn);
+      });
+    });
+
+    describe('participants', function() {
+      function responseFn(response) {
+        expect(response.data[0]).to.include({
+          record_type: 'participant',
+        });
+        expect(response.data[0]).to.have.property('conference');
+        expect(response.data[0]).to.have.property('call_control_id');
+        expect(response.data[0]).to.have.property('end_conference_on_exit');
+        expect(response.data[0]).to.have.property('muted');
+        expect(response.data[0]).to.have.property('on_hold');
+      }
+
+      it('Sends the correct request', function() {
+        return telnyx.conferences.participants(CONFERENCE_ID, {filter: {muted: true}})
+          .then(responseFn);
+      });
+
+      it('Sends the correct request [with specified auth]', function() {
+        return telnyx.conferences.participants(CONFERENCE_ID, TEST_AUTH_KEY)
           .then(responseFn);
       });
     });
