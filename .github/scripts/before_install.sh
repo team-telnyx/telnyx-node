67be3d4f011806fc2260a60c2cefbccfcfea0de3
Drew Kitch <drew@telnyx.com>
2021-12-16 23:05:56 -0700

    Update before_install.sh

--- .github/scripts/before_install.sh
+++ .github/scripts/before_install.sh
@@ -3,5 +3,5 @@ npm install -g @stoplight/prism-cli
 echo "Starting up Prism Mock Server with spec file at ${TELNYX_MOCK_OPEN_API_URI}"
 prism mock ${TELNYX_MOCK_OPEN_API_URI} > /dev/null &
 
-npm i https://github.com/team-telnyx/telnyx-mock-server-proxy --prefix ./the-proxy
-node ./the-proxy/node_modules/telnyx-mock-server-proxy/index.js > /dev/null &
+npm i https://github.com/team-telnyx/telnyx-prism-mock --prefix ./the-proxy
+node ./the-proxy/node_modules/telnyx-mock-server-proxy/proxy/index.js > /dev/null &
