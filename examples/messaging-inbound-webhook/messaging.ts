5d27aa5f629388b6b7d5543102aeff307b001963
Lucas Rosa <lucas@telnyx.com>
2025-02-17 15:29:05 -0300

    add messaging-inbound-webhook example

---
+++ examples/messaging-inbound-webhook/messaging.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import Telnyx from 'telnyx';
+
+const router = express.Router();
+
+router
+  .route('/inbound')
+  .post(async function inboundMessageController(req, res) {
+    res.sendStatus(200); // Play nice and respond to webhook
+    const event = (req.body as Telnyx.events.InboundMessageEvent).data;
+
+    if (event?.payload) {
+      console.log(`Received inbound message with ID: ${event?.payload?.id}`);
+      const toNumber = event.payload.to?.at(0)?.phone_number as string;
+      const fromNumber = event.payload.from?.phone_number as string;
+      const type = event.payload.type;
+      const text = event.payload.text;
+
+      console.log('Type: ', type);
+      console.log('Text: ', text);
+      console.log('From: ', fromNumber);
+      console.log('To: ', toNumber);
+    } else {
+      console.error('Failed to receive webhook payload');
+      console.error(JSON.stringify(event?.payload, null, 2));
+    }
+  });
+
+export default router;
